16df90d48c984bd10e130cec6fa553db8e93c568
DimRev <dimarev444@gmail.com>
2024-01-03 21:47:21 +0200

    Add Outlet of child route to work WorkSpacePage

--- src/pages/WorkSpacePage.jsx
+++ src/pages/WorkSpacePage.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import { WorkSpaceHeader } from "../cmps/WorkSpaceCmps/WorkSpaceHeader";
 
 export function WorkSpacePage() {
   return (
     <section className="page work-space-page">
       <WorkSpaceHeader />
+      <Outlet />
 
 
     </section>
