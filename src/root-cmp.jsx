2088c2e91b469b2f736f7365a6daac94973ccf37
DimRev <dimarev444@gmail.com>
2024-01-03 21:43:19 +0200

    Set WorkSpaceBoard child-routing

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -2,20 +2,19 @@ import './assets/style/main.scss'
 import { WorkSpacePage } from './pages/WorkSpacePage'
 import { HomePage } from './pages/HomePage'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { WorkSpaceBoard } from './cmps/WorkSpaceCmps/WorkSpaceBoard'
 
 export function App() {
-
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/boards" element={<WorkSpacePage />} />
+          <Route path="/workspace" element={<WorkSpacePage />}>
+            <Route index element={<WorkSpaceBoard />} />
+          </Route>
         </Routes>
       </Router>
     </>
   )
 }
-
-
