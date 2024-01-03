8ed33c6fedaa41acf894a248e7081bf5a88670f5
DimRev <dimarev444@gmail.com>
2024-01-03 21:45:53 +0200

    Add BoardTabs for WorkSpaceBoard

---
+++ src/cmps/WorkSpaceBoardCmps/BoardTabs.jsx
@@ -0,0 +1,11 @@
+export function BoardTabs({}) {
+  return <section className="board-tabs">
+        <div className="tabs">
+          <button>Main Table</button>
+          <button>add tab +</button>
+        </div>
+        <div className="expend-collapse">
+          <button>collapse/expend</button>
+        </div>
+      </section>;
+}
