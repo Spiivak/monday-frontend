edc6d1e9e1b7b8e2cb078bf6d55af49685d794d7
DimRev <dimarev444@gmail.com>
2024-01-04 23:09:15 +0200

    Change to dropdown menus + styled the status btns colors

--- src/cmps/WorkSpaceBoardCmps/DynamicTableCell.jsx
+++ src/cmps/WorkSpaceBoardCmps/DynamicTableCell.jsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useState } from "react"
-import { utilService } from "../../services/util.service"
-import { DatePicker } from "antd"
-import dayjs from "dayjs"
+import { useEffect, useRef, useState } from 'react'
+import { utilService } from '../../services/util.service'
+import { DatePicker } from 'antd'
+import dayjs from 'dayjs'
+
+import { Button, Dropdown } from 'antd'
 
 export function DynamicTableCell({ cmp, task, onTaskUpdate }) {
   function handleUpdateTask(cmpType, data, task) {
@@ -9,44 +11,92 @@ export function DynamicTableCell({ cmp, task, onTaskUpdate }) {
   }
 
   switch (cmp) {
-    case "StatusPicker":
+    case 'StatusPicker':
       return <StatusPicker {...{ task, handleUpdateTask }} />
-    case "MemberPicker":
+    case 'MemberPicker':
       return <MemberPicker {...{ task, handleUpdateTask }} />
-    case "DatePicker":
+    case 'DatePicker':
       return <DatePickerC {...{ task, handleUpdateTask }} />
-    case "DescriptionPicker":
+    case 'DescriptionPicker':
       return <DescriptionPicker {...{ task, handleUpdateTask }} />
-    case "TimeLinePicker":
+    case 'TimeLinePicker':
       return <TimeLinePicker {...{ task, handleUpdateTask }} />
   }
 }
 
 function StatusPicker({ task, handleUpdateTask }) {
-  const [isActive, setIsActive] = useState(false)
-
-  return (
-    <div
-      onClick={() => {
-        setIsActive((a) => !a)
-      }}
-      className="cell"
-    >
-      <h4>{task.status || "empty"}</h4>
-      <div className={`cell-context ${isActive ? "active" : "hidden"}`}>
-        <button onClick={() => handleUpdateTask("StatusPicker", "done", task)}>
+  const items = [
+    {
+      key: '1',
+      backgroundColor: 'green',
+      status: 'done',
+      label: (
+        <button
+          className="label-btn"
+          style={{ backgroundColor: 'green' }}
+          onClick={() => handleUpdateTask('StatusPicker', 'done', task)}>
           done
         </button>
+      ),
+    },
+    {
+      key: '2',
+      backgroundColor: 'orange',
+      status: 'in-progress',
+      label: (
         <button
-          onClick={() => handleUpdateTask("StatusPicker", "in-progress", task)}
-        >
+          className="label-btn"
+          style={{ backgroundColor: 'orange' }}
+          onClick={() => handleUpdateTask('StatusPicker', 'in-progress', task)}>
           in-progress
         </button>
-        <button onClick={() => handleUpdateTask("StatusPicker", "stuck", task)}>
+      ),
+    },
+    {
+      key: '3',
+      backgroundColor: 'red',
+      status: 'stuck',
+      label: (
+        <button
+          className="label-btn"
+          style={{ backgroundColor: 'red' }}
+          onClick={() => handleUpdateTask('StatusPicker', 'stuck', task)}>
           stuck
         </button>
+      ),
+    },
+    {
+      type: 'divider',
+    },
+    {
+      key: '4',
+      label: <button className="label-btn">Edit Labels</button>,
+    },
+  ]
+  const bgc = items.reduce((acc,item) => {
+    if (item.status === task.status) {
+      return item.backgroundColor
+    }
+    return acc
+  },'')
+
+  return (
+    <Dropdown
+      rootClassName="dropdown-status-picker"
+      menu={{
+        items,
+      }}
+      trigger={['click']}
+      placement="bottom"
+      arrow={{
+        pointAtCenter: true,
+      }}>
+      <div className="cell">
+        <button className="label-btn" style={{ backgroundColor: bgc }}>
+          {task.status || 'empty'}
+        </button>
       </div>
-    </div>
+    </Dropdown>
   )
 }
 
@@ -77,75 +127,86 @@ function MemberPicker({ task, handleUpdateTask }) {
     }, 500)
   }
 
-  function handleClick() {
-    setIsActive((a) => !a)
-  }
+  const items = [
+    {
+      key: '1',
+      label: <h4>labels</h4>,
+    },
+    {
+      key: '2',
+      label: <input type="text"></input>,
+    },
+    {
+      key: '3',
+      label: <h4>suggested people</h4>,
+    },
+    {
+      key: '4',
+      label: <button>Avatars</button>,
+    },
+  ]
+
   return (
-    <div
-      onMouseEnter={handleHover}
-      onMouseLeave={handleHoverEnd}
-      onClick={handleClick}
-      className="cell"
-    >
-      {(!!task?.members && (
-        <div className="avatars-wrapper">
-          {task.members.map((member) => (
-            <div className="avatar-logo" key={member._id}>
-              <img
-                onMouseEnter={() => handleHover(member)}
-                src={member.imgUrl}
-                alt=""
-              />
-            </div>
-          ))}
-        </div>
-      )) ||
-        "empty"}
-      {!!selectedMember && (
-        <div
-          onMouseEnter={() => (shouldActiveRef.current = true)}
-          onMouseLeave={() => (shouldActiveRef.current = false)}
-          className="member-details-wrapper"
-        >
-          <div className={`member-details`}>
-            <div className="avatar-logo">
-              <img src={selectedMember.imgUrl} alt="" />
-            </div>
-            <div className="member-details-content">
-              <h4>{selectedMember.fullname}</h4>
-              <p>[logo] Time at current location, location</p>
-              <h4>membership label</h4>
-            </div>
-            <div className="member-detail-btns">
-              <button>Btn 1</button>
-              <button>Btn 2</button>
+    <Dropdown
+      menu={{
+        items,
+      }}
+      trigger={['click']}
+      placement="bottom"
+      arrow={{
+        pointAtCenter: true,
+      }}>
+      <div
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHoverEnd}
+        className="cell">
+        {(!!task?.members && (
+          <div className="avatars-wrapper">
+            {task.members.map((member) => (
+              <div className="avatar-logo" key={member._id}>
+                <img
+                  onMouseEnter={() => handleHover(member)}
+                  src={member.imgUrl}
+                  alt=""
+                />
+              </div>
+            ))}
+          </div>
+        )) ||
+          'empty'}
+        {!!selectedMember && (
+          <div
+            onMouseEnter={() => (shouldActiveRef.current = true)}
+            onMouseLeave={() => (shouldActiveRef.current = false)}
+            className="member-details-wrapper">
+            <div className={`member-details`}>
+              <div className="avatar-logo">
+                <img src={selectedMember.imgUrl} alt="" />
+              </div>
+              <div className="member-details-content">
+                <h4>{selectedMember.fullname}</h4>
+                <p>[logo] Time at current location, location</p>
+                <h4>membership label</h4>
+              </div>
+              <div className="member-detail-btns">
+                <button>Btn 1</button>
+                <button>Btn 2</button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      <div
-        className={`member-add-selector ${isActive ? "active" : "hidden"}`}
-        onClick={(e) => {
-          e.stopPropagation()
-        }}
-      >
-        <div className="member-labels">labels</div>
-        <div className="member-search">
-          <input type="text" placeholder="search" />
-        </div>
-        <div className="member-suggestions">suggested members</div>
+        )}
       </div>
-    </div>
+    </Dropdown>
   )
 }
 
 function DatePickerC({ task, handleUpdateTask }) {
-  const dateFormat = "YYYY/MM/DD"
+  const dateFormat = 'YYYY/MM/DD'
 
   function handleUpdateDate(date) {
     if (date) {
       const timestampDate = date.valueOf()
-      handleUpdateTask("DatePicker", timestampDate, task)
+      handleUpdateTask('DatePicker', timestampDate, task)
     }
   }
   return (
@@ -177,17 +238,17 @@ function DescriptionPicker({ task, handleUpdateTask }) {
   function removeLeadingSlashN(description) {
     let modifiedDescription = description
 
-    while (modifiedDescription && modifiedDescription.startsWith("\n")) {
+    while (modifiedDescription && modifiedDescription.startsWith('\n')) {
       modifiedDescription = modifiedDescription.slice(2)
     }
 
     return modifiedDescription
   }
 
-  const descriptionToShow = removeLeadingSlashN(desc) || "empty"
+  const descriptionToShow = removeLeadingSlashN(desc) || 'empty'
 
   useEffect(() => {
-    handleUpdateTask.current("DescriptionPicker", desc, task)
+    handleUpdateTask.current('DescriptionPicker', desc, task)
   }, [desc])
 
   return (
@@ -195,11 +256,10 @@ function DescriptionPicker({ task, handleUpdateTask }) {
       <h4
         onClick={() => {
           setIsActive((a) => !a)
-        }}
-      >
+        }}>
         {descriptionToShow}
       </h4>
-      <div className={`cell-context ${isActive ? "active" : "hidden"}`}>
+      <div className={`cell-context ${isActive ? 'active' : 'hidden'}`}>
         <textarea
           placeholder="Add description"
           value={desc}
@@ -215,7 +275,7 @@ function DescriptionPicker({ task, handleUpdateTask }) {
 
 function TimeLinePicker({ task, handleUpdateTask }) {
   const { RangePicker } = DatePicker
-  const dateFormat = "YYYY/MM/DD"
+  const dateFormat = 'YYYY/MM/DD'
 
   function handleDateChange(dates) {
     if (dates) {
@@ -224,7 +284,7 @@ function TimeLinePicker({ task, handleUpdateTask }) {
       const timestampEndDate = endDate.valueOf()
 
       handleUpdateTask(
-        "TimeLinePicker",
+        'TimeLinePicker',
         [timestampStartDate, timestampEndDate],
         task
       )
