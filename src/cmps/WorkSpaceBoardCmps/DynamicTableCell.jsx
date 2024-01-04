df0a8dc8a3a1d7b2213966d73376335b461ded9e
DimRev <dimarev444@gmail.com>
2024-01-04 15:43:42 +0200

    Edit dynamic cell -> members cell works displays members on hover

--- src/cmps/WorkSpaceBoardCmps/DynamicTableCell.jsx
+++ src/cmps/WorkSpaceBoardCmps/DynamicTableCell.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export function DynamicTableCell({ cmp, task, onTaskUpdate }) {
-  function handleUpdateTask(cmpType ,data, task) {
+  function handleUpdateTask(cmpType, data, task) {
     onTaskUpdate(cmpType, data, task)
   }
 
@@ -26,28 +26,100 @@ function StatusPicker({ task, handleUpdateTask }) {
       className="cell">
       <h4>{task.status || 'empty'}</h4>
       <div className={`cell-context ${isActive ? 'active' : 'hidden'}`}>
-        <button onClick={() => handleUpdateTask('StatusPicker','done',task)}>done</button>
-        <button onClick={() => handleUpdateTask('StatusPicker','in-progress',task)}>
+        <button onClick={() => handleUpdateTask('StatusPicker', 'done', task)}>
+          done
+        </button>
+        <button
+          onClick={() => handleUpdateTask('StatusPicker', 'in-progress', task)}>
           in-progress
         </button>
-        <button onClick={() => handleUpdateTask('StatusPicker','stuck',task)}>stuck</button>
+        <button onClick={() => handleUpdateTask('StatusPicker', 'stuck', task)}>
+          stuck
+        </button>
       </div>
     </div>
   )
 }
 
-function MemberPicker({ task, onTaskUpdate }) {
+function MemberPicker({ task, handleUpdateTask }) {
+  const [isActive, setIsActive] = useState(false)
+  const [selectedMember, setSelectedMember] = useState(null)
+  const hoverTimeoutRef = useRef(null)
+  const hoverEndTimeoutRef = useRef(null)
+  const shouldActiveRef = useRef(false)
+
+  function handleHover(member) {
+    clearTimeout(hoverEndTimeoutRef.current)
+    shouldActiveRef.current = true
+    hoverTimeoutRef.current = setTimeout(() => {
+      if (shouldActiveRef.current) {
+        setSelectedMember(member)
+        setIsActive(true)
+      }
+    }, 1000)
+  }
+
+  function handleHoverEnd() {
+    clearTimeout(hoverTimeoutRef.current)
+    shouldActiveRef.current = false
+    hoverEndTimeoutRef.current = setTimeout(() => {
+      if (!shouldActiveRef.current) {
+        setSelectedMember(null)
+        setIsActive(false)
+      }
+    }, 500)
+  }
+
+  function handleClick() {}
   return (
-    <button>
-      <h4>{task.memberIds}</h4>
-    </button>
+    <div
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHoverEnd}
+      onClick={handleClick}
+      className="cell">
+      {(!!task?.members && (
+        <div className="avatars-wrapper">
+          {task.members.map((member) => (
+            <div
+              onMouseEnter={() => handleHover(member)}
+              className="avatar-logo"
+              key={member._id}>
+              <img src={member.imgUrl} alt="" />
+            </div>
+          ))}
+        </div>
+      )) ||
+        'empty'}
+      {!!selectedMember && (
+        <div
+          onMouseEnter={() => (shouldActiveRef.current = true)}
+          onMouseLeave={() => (shouldActiveRef.current = false)}
+          className="member-details-wrapper">
+          <div className={`member-details ${isActive ? 'active' : 'hidden'}`}>
+            <div className="avatar-logo">
+              <img src={selectedMember.imgUrl} alt="" />
+            </div>
+            <div className="member-details-content">
+              <h4>{selectedMember.fullname}</h4>
+              <p>[logo] Time at current location, location</p>
+              <h4>membership label</h4>
+            </div>
+            <div className="member-detail-btns">
+              <button>Btn 1</button>
+              <button>Btn 2</button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="member-add-selector"></div>
+    </div>
   )
 }
 
-function DatePicker({ task, onTaskUpdate }) {
+function DatePicker({ task, handleUpdateTask }) {
   return (
-    <button>
-      <h2>{task.date}</h2>
-    </button>
+    <div className="cell">
+      <h4>{task.date || 'empty'}</h4>
+    </div>
   )
 }
