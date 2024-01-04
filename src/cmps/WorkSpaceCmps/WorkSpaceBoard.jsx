49ecf6c730d097c0fc6f473d480b9d92459ffb14
NaveDavid19 <navedavid16@gmail.com>
2024-01-04 10:19:04 +0200

    Update imports

--- src/cmps/WorkSpaceCmps/WorkSpaceBoard.jsx
+++ src/cmps/WorkSpaceCmps/WorkSpaceBoard.jsx
@@ -1,12 +1,12 @@
-import { BoardViewList } from '../WorkSpaceBoardCmps/BoardViewList'
-import { BoardFilter } from '../WorkSpaceBoardCmps/BoardFilter'
-import { BoardTabs } from '../WorkSpaceBoardCmps/BoardTabs'
+import { BoardViewList } from "../WorkSpaceBoardCmps/BoardViewList"
+import { BoardFilter } from "../WorkSpaceBoardCmps/BoardFilter"
+import { BoardTabs } from "../WorkSpaceBoardCmps/BoardTabs"
 
-import { BoardHeader } from '../WorkSpaceBoardCmps/BoardHeader'
-import { useEffect } from 'react'
-import { loadBoards } from '../../store/actions/board.actions'
-import { useSelector } from 'react-redux'
-import { WorkSpaceSideBar } from '../BoardCmps/WorkSpaceSideBar'
+import { BoardHeader } from "../WorkSpaceBoardCmps/BoardHeader"
+import { useEffect } from "react"
+import { loadBoards } from "../../store/actions/board.actions"
+import { useSelector } from "react-redux"
+import { WorkSpaceSideBar } from "./WorkSpaceSideBar"
 
 export function WorkSpaceBoard() {
   const boards = useSelector((storeState) => storeState.boardModule.boards)
@@ -23,7 +23,7 @@ export function WorkSpaceBoard() {
         <BoardHeader />
         <BoardTabs />
         <BoardFilter />
-      {!!boards && <BoardViewList board={boards[0]}/>}
+        {!!boards && <BoardViewList board={boards[0]} />}
       </section>
     </div>
   )
