f50b92706a672f345a1c06bdbe16f49ed72409ed
DimRev <dimarev444@gmail.com>
2024-01-04 09:39:10 +0200

    Add store

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -3,18 +3,22 @@ import { WorkSpacePage } from './pages/WorkSpacePage'
 import { HomePage } from './pages/HomePage'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { WorkSpaceBoard } from './cmps/WorkSpaceCmps/WorkSpaceBoard'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 export function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/workspace" element={<WorkSpacePage />}>
-            <Route index element={<WorkSpaceBoard />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/workspace" element={<WorkSpacePage />}>
+              <Route index element={<WorkSpaceBoard />} />
+            </Route>
+          </Routes>
+        </Router>
+      </Provider>
     </>
   )
 }
