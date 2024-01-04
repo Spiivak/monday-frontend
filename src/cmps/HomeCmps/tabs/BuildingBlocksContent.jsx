494c33c1abf846ebe0386d880358ef0690e2b873
Spiivak <edenspiivak@gmail.com>
2024-01-04 02:05:38 +0200

    Done with homepage

--- src/cmps/HomeCmps/tabs/BuildingBlocksContent.jsx
+++ src/cmps/HomeCmps/tabs/BuildingBlocksContent.jsx
@@ -1,38 +1,69 @@
 // BuildingBlocksContent.js
 import React from 'react';
-import Video from '../../../assets/img/7_status.mp4'
+import Boards from '../../../assets/img/7_status.mp4'
+import Views from '../../../assets/img/1_Views.mp4'
+import Integrations from '../../../assets/img/4_Integrations.mp4'
+import Dashboards from '../../../assets/img/2_Dashboards.mp4'
+import Automations from '../../../assets/img/3_Automations.mp4'
+import Apps from '../../../assets/img/5_Apps.mp4'
+import Docs from '../../../assets/img/6_Docs.mp4'
+
+
+
 
 const tabsData = {
   boards: {
+    label: 'Boards',
+    video: Boards,
     content: 'Everything starts with a visual board — the core of monday.com Work OS. Tailor it your way and manage anything from projects to departments.',
   },
   views: {
-    content: 'Content for Views tab.',
+    label: 'Views',
+    video: Views,
+    content: 'Visualize and plan your work more efficiently with multiple views: Kanban board, calendar, timeline, Gantt chart, and more.',
   },
   dashboards: {
-    content: 'Content for Dashboards tab.',
+    label: 'Dashboards',
+    video: Dashboards,
+    content: 'Get the insights you need to make decisions with confidence. Keep track of progress, timelines, and budgets with custom dashboards.',
+    link: 'Learn more'
   },
   integrations: {
-    content: 'Content for Integrations tab.',
+    label: 'Integrations',
+    video: Integrations,
+    content: 'Connect monday.com with all your favorite tools and get more work done. Integrate Slack, Dropbox, Adobe Creative Cloud, and more.',
+    link: 'Learn more'
   },
   automations: {
-    content: 'Content for Automations tab.',
+    label: 'Automations',
+    video: Automations,
+    content: 'Streamline processes to focus on the work that matters. Choose from a variety of automation recipes or create your own in minutes.',
+    link: 'Learn more'
   },
   apps: {
-    content: 'Content for Apps tab.',
+    label: 'Apps',
+    video: Apps,
+    content: 'Expand the capabilities of your Work OS with monday apps. Enhance your workflows with custom views, widgets, integrations, and more.',
+    link: 'Learn more'
   },
   docs: {
-    content: 'Content for Docs tab.',
+    label: 'Docs',
+    video: Docs,
+    content: 'Transform text into action items, in just a few clicks. Connect, collaborate, and execute ideas and workflows in real-time from any doc.',
+    link: 'Learn more'
   },
 };
 
 export function BuildingBlocksContent({ activeTab }) {
   return (
-    <div className="iframe-info">
-      <iframe src={Video}></iframe>
+    <div className="iframe-info flex gap16">
+      <div className="iframe-info-video">
+      <video src={tabsData[activeTab].video} autoPlay></video>
+      </div>
       <div className="info">
         <h2>{tabsData[activeTab].label}</h2>
         <p>{tabsData[activeTab].content}</p>
+        {tabsData[activeTab]?.link && <a href=''>{tabsData[activeTab].link}</a>}
       </div>
     </div>
   );
