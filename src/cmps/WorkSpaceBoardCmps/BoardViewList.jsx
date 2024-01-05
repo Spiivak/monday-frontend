285d5dbf378517c559cfe4c54cc5e3dbc9d6348f
DimRev <dimarev444@gmail.com>
2024-01-05 09:12:01 +0200

    Add addnew group btn

--- src/cmps/WorkSpaceBoardCmps/BoardViewList.jsx
+++ src/cmps/WorkSpaceBoardCmps/BoardViewList.jsx
@@ -1,17 +1,22 @@
+import { Add } from '@mui/icons-material'
 import { BoardViewGroup } from '../WorkSpaceBoardCmps/BoardViewGroup'
 import React from 'react'
-export function BoardViewList({ board }) {
+export function BoardViewList({ board, onAddGroup }) {
   return (
     <section className="board-view-list">
-      {!!board &&
-        board.groups.map((group) => (
-          <BoardViewGroup
-            boardId={board._id}
-            key={group.id}
-            group={group}
-            cmpsOrder={board.cmpsOrder}
-          />
-        ))}
+      {!!board && (
+        <>
+          {board.groups.map((group) => (
+            <BoardViewGroup
+              boardId={board._id}
+              key={group.id}
+              group={group}
+              cmpsOrder={board.cmpsOrder}
+            />
+          ))}
+          <button className='flex' onClick={()=>onAddGroup(board._id)}><Add/> Add new group</button>
+        </>
+      )}
     </section>
   )
 }
