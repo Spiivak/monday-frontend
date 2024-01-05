e92b73e329ff888a60c7522e2c216a0010faa8c7
DimRev <dimarev444@gmail.com>
2024-01-05 11:57:36 +0200

    Add context-btn to board navs

--- src/cmps/WorkSpaceSideBarCmps/WorkSpaceList.jsx
+++ src/cmps/WorkSpaceSideBarCmps/WorkSpaceList.jsx
@@ -1,28 +1,23 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { BoardIcon, DeleteIcon } from '../Icons'
+import { ContextBtn } from '../ContextBtn'
 
 export function WorkSpaceList({ onRemoveBoard }) {
   const boards = useSelector((storeState) => storeState.boardModule.boards)
-
   return (
     <div className="workspaces-list flex column">
       {boards.map((board) => (
         <div key={board._id}>
           <div className="workspace-item flex align-center space-between">
-            <div className="link">
-              <Link to={`/workspace/${board._id}`} className="flex gap8 align-center">
-                <BoardIcon />
-                {board.title}
-              </Link>
-            </div>
-            <div className="menu-btn">
-              <button
-                className="btn-icon medium-transparent"
-                onClick={() => onRemoveBoard(board._id)}
-              >
-                <DeleteIcon />
-              </button>
+            <div className="link flex space-between">
+              <NavLink to={`/workspace/${board._id}`} className="flex gap8 align-center hoverable space-between">
+                <div className="nav-link-content flex align-center gap8">
+                  <BoardIcon />
+                  {board.title}
+                </div>
+            <ContextBtn type='board' onDeleteBoard={()=>onRemoveBoard(board._id)}/>
+              </NavLink>
             </div>
           </div>
         </div>
