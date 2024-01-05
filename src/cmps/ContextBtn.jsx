3a95ccafe105e65bedd53b70de1a2c803df37b96
DimRev <dimarev444@gmail.com>
2024-01-05 11:55:46 +0200

    add context-board dynamic menus

--- src/cmps/ContextBtn.jsx
+++ src/cmps/ContextBtn.jsx
@@ -16,10 +16,12 @@ import {
 } from './Icons'
 import Mwm from '../assets/img/mwm.png'
 import { Save } from '@mui/icons-material'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export function ContextBtn({ type, onDeleteGroup }) {
+export function ContextBtn({ type, onDeleteGroup, onDeleteBoard }) {
+  const menuBtnRef = useRef()
   const [isMoreModalOpen, setIsMoreModalOpen] = useState(false)
+
   function openMoreModal() {
     setIsMoreModalOpen((isOpen) => !isOpen)
   }
@@ -29,11 +31,36 @@ export function ContextBtn({ type, onDeleteGroup }) {
       return (
         <div className="context-menu relative">
           <button
+            ref={menuBtnRef}
             className="btn-icon small-transparent"
             onClick={() => openMoreModal()}>
             <MenuIcon />
           </button>
-          {isMoreModalOpen && <MoreModal onDeleteGroup={onDeleteGroup} />}
+          {isMoreModalOpen && (
+            <GroupModal
+              onDeleteGroup={onDeleteGroup}
+              setIsMoreModalOpen={setIsMoreModalOpen}
+              menuBtnRef={menuBtnRef}
+            />
+          )}
+        </div>
+      )
+    case 'board':
+      return (
+        <div className="context-menu relative hidden-hover">
+          <button
+            ref={menuBtnRef}
+            className="btn-icon small-transparent"
+            onClick={() => openMoreModal()}>
+            <MenuIcon />
+          </button>
+          {isMoreModalOpen && (
+            <BoardModal
+              onDeleteBoard={onDeleteBoard}
+              setIsMoreModalOpen={setIsMoreModalOpen}
+              menuBtnRef={menuBtnRef}
+            />
+          )}
         </div>
       )
     default:
@@ -41,9 +68,32 @@ export function ContextBtn({ type, onDeleteGroup }) {
   }
 }
 
-function MoreModal({ onDeleteGroup }) {
+function GroupModal({ onDeleteGroup, setIsMoreModalOpen, menuBtnRef }) {
+  const modalRef = useRef()
+
+  useEffect(() => {
+    // Add event listener to close modal when clicking outside
+    const handleOutsideClick = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target) &&
+        menuBtnRef.current &&
+        !menuBtnRef.current.contains(event.target)
+      ) {
+        setIsMoreModalOpen(false)
+      }
+    }
+
+    // Add event listener to the document body
+    document.body.addEventListener('click', handleOutsideClick)
+
+    // Remove event listener on component unmount
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick)
+    }
+  }, [setIsMoreModalOpen])
   return (
-    <div className="more-modal-container flex column">
+    <div className="more-modal-container flex column" ref={modalRef}>
       <div className="ds-tabs-section">
         <div className="tab">
           <button className="btn-icon medium-transparent flex gap16" disabled>
@@ -106,6 +156,109 @@ function MoreModal({ onDeleteGroup }) {
           <button
             className="btn-icon medium-transparent flex gap16"
             onClick={onDeleteGroup}>
+            <DeleteIcon />
+            Delete
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16">
+            <ArchiveIcon />
+            Archive
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function BoardModal({ onDeleteBoard, setIsMoreModalOpen, menuBtnRef }) {
+  const modalRef = useRef()
+
+  useEffect(() => {
+    // Add event listener to close modal when clicking outside
+    const handleOutsideClick = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target) &&
+        menuBtnRef.current &&
+        !menuBtnRef.current.contains(event.target)
+      ) {
+        setIsMoreModalOpen(false)
+      }
+    }
+
+    // Add event listener to the document body
+    document.body.addEventListener('click', handleOutsideClick)
+
+    // Remove event listener on component unmount
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick)
+    }
+  }, [setIsMoreModalOpen])
+  return (
+    <div className="more-modal-container flex column" ref={modalRef}>
+      <div className="ds-tabs-section">
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16" disabled>
+            <EditIcon />
+            Rename workspace
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16" disabled>
+            <SwitchIcon />
+            Change icon
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16">
+            <SettingsIcon />
+            Manage workspace
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16" disabled>
+            <Save />
+            Save as template
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16" disabled>
+            <DeleteIcon />
+            Delete workspace
+          </button>
+        </div>
+      </div>
+      <div className="ds-divider"></div>
+      <div className="ds-tabs-section">
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16">
+            <AddSmallIcon />
+            Add new workspace
+          </button>
+        </div>
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16">
+            <AddSmallIcon />
+            Browse all workspaces
+          </button>
+        </div>
+      </div>
+      <div className="ds-divider"></div>
+      <div className="ds-tabs-section">
+        <div className="tab">
+          <button className="btn-icon medium-transparent flex gap16">
+            <CollapseIcon />
+            Collapse all folders
+          </button>
+        </div>
+      </div>
+      <div className="ds-divider"></div>
+      <div className="ds-tabs-section">
+        <div className="tab">
+          <button
+            className="btn-icon medium-transparent flex gap16"
+            onClick={onDeleteBoard}>
             <DeleteIcon />
             Delete
           </button>
