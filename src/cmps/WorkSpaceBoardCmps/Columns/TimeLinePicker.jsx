e7bbc7712e0288e7dc526f541170021b5917f5b2
NaveDavid19 <navedavid16@gmail.com>
2024-01-07 20:20:31 +0200

    Refactor TimeLinePicker

--- src/cmps/WorkSpaceBoardCmps/Columns/TimeLinePicker.jsx
+++ src/cmps/WorkSpaceBoardCmps/Columns/TimeLinePicker.jsx
@@ -1,34 +1,82 @@
 import { DatePicker } from 'antd'
 import dayjs from 'dayjs'
+import { useState } from 'react'
 
 export function TimeLinePicker({ task, cmpId, handleUpdateTask }) {
+  const [dateModal, setDateModal] = useState(false)
+
   const { RangePicker } = DatePicker
   const dateFormat = 'YYYY/MM/DD'
 
-  function handleDateChange(dates) {
+  async function handleDateChange(dates) {
     if (dates) {
-      const [startDate, endDate] = dates
-      const timestampStartDate = startDate.valueOf()
-      const timestampEndDate = endDate.valueOf()
-
-      handleUpdateTask(
-        'TimeLinePicker',
-        [timestampStartDate, timestampEndDate],
-        task
-      )
+      try {
+        const [startDate, endDate] = dates
+        const timestampStartDate = startDate.valueOf()
+        const timestampEndDate = endDate.valueOf()
+        await handleUpdateTask(
+          'TimeLinePicker',
+          [timestampStartDate, timestampEndDate],
+          task
+        )
+      } catch (err) {
+        console.log('cannot set dates', err)
+      } finally {
+        setDateModal(false)
+      }
     }
+  }
+
+  function formatDateRange(timestamps) {
+    if (timestamps.length !== 2) {
+      throw new Error('Expected an array of two timestamps')
+    }
+
+    const [startTimestamp, endTimestamp] = timestamps
+    const startDate = new Date(startTimestamp)
+    const endDate = new Date(endTimestamp)
+
+    const startDayNumber = startDate.getDate()
+    const endDayNumber = endDate.getDate()
+    const startMonthAbbreviation = startDate.toLocaleString('default', {
+      month: 'short',
+    })
+    const endMonthAbbreviation = endDate.toLocaleString('default', {
+      month: 'short',
+    })
+
+    return `${startDayNumber} ${startMonthAbbreviation} - ${endDayNumber} ${endMonthAbbreviation}`
   }
 
   return (
     <div className="cell">
-      {task['timeline'+cmpId] ? (
-        <RangePicker
-          defaultValue={task['timeline'+cmpId].map((timestamp) => dayjs(timestamp))}
-          format={dateFormat}
-          onChange={handleDateChange}
-        />
+      {!dateModal ? (
+        <div
+          className="bill"
+          onClick={() => {
+            setDateModal(true)
+          }}
+        >
+          {task['timeline' + cmpId]
+            ? formatDateRange(task['timeline' + cmpId])
+            : '-'}
+        </div>
       ) : (
-        <RangePicker format={dateFormat} onChange={handleDateChange} />
+        <div className="bill">
+          <RangePicker
+            style={{ opacity: '0', padding: '0' }}
+            onClick={() => setDateModal(true)}
+            onOpenChange={() => setDateModal((a) => !a)}
+            format={dateFormat}
+            onChange={(ev) => handleDateChange(ev)}
+            defaultValue={
+              task['timeline' + cmpId] ? dayjs(task['timeline' + cmpId]) : null
+            }
+            open={dateModal}
+            autoFocus={dateModal}
+          />
+          //{' '}
+        </div>
       )}
     </div>
   )
