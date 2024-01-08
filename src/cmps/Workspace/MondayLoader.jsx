701757bb5ad85a5fa87b3f43746b210524baf48b
Spiivak <edenspiivak@gmail.com>
2024-01-08 22:58:09 +0200

    Add loader

--- src/cmps/Workspace/MondayLoader.jsx
+++ src/cmps/Workspace/MondayLoader.jsx
@@ -1,6 +1,6 @@
-import Loader from '../../assets/img/board-loader.gif'
-
+import React from 'react';
+import Loader from '../../assets/img/board-loader.gif';
 
 export function MondayLoader() {
-  return <img src={Loader} />
+  return <img src={Loader} alt="Monday Loader" />;
 }
