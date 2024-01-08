f64158bb0d47b3ce5a0f059d46697d45b11f98d9
NaveDavid19 <navedavid16@gmail.com>
2024-01-08 18:45:29 +0200

    Styling filePicker cell

--- src/cmps/ImgUploader.jsx
+++ src/cmps/ImgUploader.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { uploadService } from '../services/upload.service'
 import { styled } from '@mui/material/styles'
-import UploadFileIcon from '@mui/icons-material/UploadFile'
-import CloseIcon from '@mui/icons-material/Close'
+import { CloseSmallIcon } from './Icons'
 import { IconButton } from '@mui/material'
 import { CircularProgress } from '@mui/material'
 import { ImageModal } from './Workspace/board/groups/cells/modals/ImageModal'
+import { FileIcon } from './Icons'
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
   clipPath: 'inset(50%)',
@@ -49,59 +49,97 @@ export function ImgUploader({ imgData, handleUpdateTaskFile = null }) {
   }
 
   return (
-    <div className="upload-preview cell" style={{ padding: '7px' }}>
+    <div
+      className="upload-preview cell flex space-between"
+      style={{ padding: '7px' }}
+    >
       {isLoading ? (
-        <CircularProgress size="16px" />
+        <CircularProgress size="16px" style={{ margin: 'auto' }} />
       ) : (
         <>
           {imgData?.imgUrl ? (
             <>
               <IconButton
+                sx={{ '&:hover': { backgroundColor: 'transparent' } }}
+                className="hidden-hover"
                 component="label"
                 size="small"
-                style={{ padding: '0px' }}
+                style={{
+                  padding: '0px',
+                }}
               >
-                <UploadFileIcon fontSize="small" />
                 <VisuallyHiddenInput
                   type="file"
                   onChange={uploadImg}
                   accept="img/*"
                   id="imgUpload"
                 />
+                <div
+                  style={{ padding: '0px', margin: 'auto' }}
+                  className="hidden-hover flex align-center"
+                >
+                  <button
+                    className="hidden-hover btn-ctn small-primary flex "
+                    style={{
+                      padding: '0px',
+                      borderRadius: '50%',
+                      width: '14px',
+                      height: '14px',
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
               </IconButton>
               <div className="image-preview flex align-center">
                 <img
                   src={imgData.imgUrl}
                   onMouseEnter={() => setIsModalOpen(true)}
-                  // onMouseLeave={() => setIsModalOpen(false)}
                 />
                 <div className="modal">
                   <ImageModal src={imgData} />
                 </div>
               </div>
-              <IconButton
+              <button
                 onClick={removeImg}
-                component="label"
-                size="small"
-                style={{ padding: '0px' }}
+                style={{ padding: '0px', borderRadius: '50%' }}
+                className="hidden-hover btn-ctn small-sec flex"
               >
-                <CloseIcon fontSize="small" />
-              </IconButton>
+                <span className="flex align-center">
+                  <CloseSmallIcon />
+                </span>
+              </button>
             </>
           ) : (
-            <IconButton
-              component="label"
-              size="small"
-              style={{ padding: '0px' }}
+            <label
+              style={{ width: '100%' }}
+              className="flex justify-center relative hidden-hover"
+              htmlFor="imgUpload"
             >
-              <UploadFileIcon fontSize="small" />
-              <VisuallyHiddenInput
-                type="file"
-                onChange={uploadImg}
-                accept="img/*"
-                id="imgUpload"
-              />
-            </IconButton>
+              <div style={{ position: 'absolute', top: '-37%' }}>
+                <FileIcon className="relative flex justify-center " />
+              </div>
+              <button
+                className="hidden-hover btn-ctn small-primary flex absolute"
+                style={{
+                  padding: '0px',
+                  borderRadius: '50%',
+                  width: '12px',
+                  height: '12px',
+                  right: '5%',
+                  top: '15%',
+                  translate: '0 40%',
+                }}
+              >
+                +
+                <VisuallyHiddenInput
+                  type="file"
+                  onChange={uploadImg}
+                  accept="img/*"
+                  id="imgUpload"
+                />
+              </button>
+            </label>
           )}
         </>
       )}
