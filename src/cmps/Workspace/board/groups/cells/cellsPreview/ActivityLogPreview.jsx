d68aae8d1911472002b3061204e6dd63ad15fb57
Spiivak <edenspiivak@gmail.com>
2024-01-10 02:31:56 +0200

    Add design to taskDetails

--- src/cmps/Workspace/board/groups/cells/cellsPreview/ActivityLogPreview.jsx
+++ src/cmps/Workspace/board/groups/cells/cellsPreview/ActivityLogPreview.jsx
@@ -1,33 +1,24 @@
 import { useSelector } from 'react-redux'
 import { utilService } from '../../../../../../services/util.service'
-import { TimeIcon } from '../../../../../Icons'
-
+import { NavigationChevronDownIcon, PersonRoundedIcon, TimeIcon } from '../../../../../Icons'
+import Frame from '../../../../../../assets/img/Frame.png'
 export function ActivityLogPreview() {
-  const selectedTask = useSelector(
-    (storeState) => storeState.boardModule.activeTask
-  )
+  const selectedTask = useSelector((storeState) => storeState.boardModule.activeTask)
 
   return (
-    <div>
+    <div className='activity-log flex column'>
       {selectedTask.activity?.map((active) => (
-        <div className="flex gap8" key={active.createdAt}>
-          <span>
+        <div className="flex" key={active.createdAt}>
+          <span className='time-format'>
             <TimeIcon />
             {utilService.timeDiff(active.createdAt)}
           </span>
-          <div>{active.title}</div>
-          <div>{active.colName}</div>
-          <div>{active.oldValue}</div>
-          <span>&gt;</span>
-          <div
-            style={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-            }}
-          >
-            {active.newValue}
-          </div>
+          <span className='active-userimg'><PersonRoundedIcon/></span>
+          <span className='active-title'>{active.title}</span>
+          <span className='active-colname'>{active.colName}</span>
+          <span className='old-value'>{active.oldValue}</span>
+          <span className='chevron'><NavigationChevronDownIcon/></span>
+          <span className='new-value'>{active.newValue}</span>
         </div>
       ))}
     </div>
