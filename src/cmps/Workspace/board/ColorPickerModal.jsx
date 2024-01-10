e05ef42c28518ece8a78ded0b4ef48bd82eddc64
NaveDavid19 <navedavid16@gmail.com>
2024-01-10 14:35:35 +0200

    Split ColorPicker to cmp

--- src/cmps/Workspace/board/ColorPickerModal.jsx
+++ src/cmps/Workspace/board/ColorPickerModal.jsx
@@ -1,16 +1,46 @@
-export function ColorPickerModal({ colors, onColorClick }) {
+export function ColorPickerModal({ handleColor }) {
+  const optinalcolors = [
+    '#ffcb00',
+    '#007038',
+    '#469e9b',
+    '#579bfc',
+    '#9aadbd',
+    '#bba5e8',
+    '#8050ab',
+    '#4f3a65',
+    '#92334c',
+    '#bb3354',
+    '#ff7575',
+  ]
   return (
-    <div className="color-picker-modal">
-      {colors.map((color, idx) => {
-        return (
-          <div
-            key={idx}
-            className="color"
-            onClick={() => onColorClick(color)}
-            style={{ backgroundColor: color }}
-          ></div>
-        )
-      })}
+    <div
+      className="flex wrap gap8"
+      style={{
+        cursor: 'default',
+        width: '150px',
+        height: '150px',
+        backgroundColor: 'white',
+        position: 'absolute',
+        border: '1px solid black',
+        top: '40px',
+        left: '0px',
+        padding: '8px',
+        zIndex: 10000,
+      }}
+    >
+      {optinalcolors.map((color, index) => (
+        <div
+          onClick={() => handleColor(color)}
+          key={index}
+          style={{
+            cursor: 'pointer',
+            width: '25px',
+            height: '25px',
+            borderRadius: '7px',
+            backgroundColor: color,
+          }}
+        ></div>
+      ))}
     </div>
   )
 }
--- src/cmps/Workspace/board/editableText/groups/GroupTitle.jsx
+++ src/cmps/Workspace/board/editableText/groups/GroupTitle.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from 'react'
+import { ColorPickerModal } from '../../ColorPickerModal'
 
 export function GroupTitle({
   initialText,
@@ -7,19 +8,6 @@ export function GroupTitle({
   type,
   textColor,
 }) {
-  const optinalcolors = [
-    '#ffcb00',
-    '#007038',
-    '#469e9b',
-    '#579bfc',
-    '#9aadbd',
-    '#bba5e8',
-    '#8050ab',
-    '#4f3a65',
-    '#92334c',
-    '#bb3354',
-    '#ff7575',
-  ]
   const editableTextRef = useRef()
   const textInputRef = useRef()
   const [inputText, setInputText] = useState('')
@@ -36,6 +24,7 @@ export function GroupTitle({
 
   function handleColor(color) {
     onSave({ prop: 'style', value: { color: color } })
+    setColorPicker(!colorPicker)
   }
 
   useEffect(() => {
@@ -72,37 +61,7 @@ export function GroupTitle({
         ref={editableTextRef}
         className="group-title-input"
       >
-        {colorPicker && (
-          <div
-            className="flex wrap gap8"
-            style={{
-              cursor: 'default',
-              width: '150px',
-              height: '150px',
-              backgroundColor: 'white',
-              position: 'absolute',
-              border: '1px solid black',
-              top: '40px',
-              left: '0px',
-              padding: '8px',
-              zIndex: 10000,
-            }}
-          >
-            {optinalcolors.map((color, index) => (
-              <div
-                onClick={() => handleColor(color)}
-                key={index}
-                style={{
-                  cursor: 'pointer',
-                  width: '25px',
-                  height: '25px',
-                  borderRadius: '7px',
-                  backgroundColor: color,
-                }}
-              ></div>
-            ))}
-          </div>
-        )}
+        {colorPicker && <ColorPickerModal {...{ handleColor }} />}
         <div style={{ position: 'relative' }}>
           <button
             style={{
