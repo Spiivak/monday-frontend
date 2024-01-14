60cdda8d40533ca90fc0d4a4558a639b84169777
DimRev <dimarev444@gmail.com>
2024-01-14 08:59:37 +0200

    Added stylized data to activity log

--- src/cmps/Workspace/board/groups/cells/StatusPicker.jsx
+++ src/cmps/Workspace/board/groups/cells/StatusPicker.jsx
@@ -1,6 +1,6 @@
-import { Edit } from '@mui/icons-material';
-import { ConfigProvider, Dropdown } from 'antd';
-import React from 'react';
+import { Edit } from '@mui/icons-material'
+import { ConfigProvider, Dropdown } from 'antd'
+import React from 'react'
 
 export function StatusPicker({
   task,
@@ -11,13 +11,21 @@ export function StatusPicker({
   loggedInUser,
 }) {
   const colName =
-    cmpsOrder.find((cmp) => cmp.type === 'StatusPicker')?.title || '';
-  const oldValue = task['status' + cmpId] || '';
+    cmpsOrder.find((cmp) => cmp.type === 'StatusPicker')?.title || ''
+  const oldValue = task['status' + cmpId] || ''
+
+  const selectedLabel = board['labels' + cmpId]?.find(
+    (label) => label.id === task['status' + cmpId]
+  )
+
+  function getLabelById(labelId) {
+    return board['labels' + cmpId]?.find((label) => label.id === labelId)
+  }
 
   async function handleUpdateStatus(status) {
     try {
-      const updatedTask = { ...task, ['status' + cmpId]: status };
-      await handleUpdateTask('StatusPicker', status, updatedTask);
+      const updatedTask = { ...task, ['status' + cmpId]: status }
+      await handleUpdateTask('StatusPicker', status, updatedTask)
       await handleUpdateTask(
         'Activity',
         {
@@ -25,36 +33,38 @@ export function StatusPicker({
           createdAt: Date.now(),
           title: updatedTask.title || '',
           colName,
-          oldValue: oldValue || '',
-          newValue: status,
+          oldValue: getLabelById(oldValue).title || 'Haven\'t Starterd',
+          oldValueColor: getLabelById(oldValue).color || '#eee',
+          newValue: getLabelById(status).title,
+          newValueColor: getLabelById(status).color,
         },
         updatedTask
-      );
+      )
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
   }
 
-  const labelItems = board['labels' + cmpId]?.map((label, idx) => ({
-    key: idx + 1,
-    backgroundcolor: label.color,
-    status: label.title,
-    label: (
-      <button
-        key={label.id}
-        className="btn-ctn medium-primary"
-        style={{
-          backgroundColor: label.color,
-          width: '152px',
-          height: '32px',
-          margin: '10px 10px 0 10px',
-        }}
-        onClick={() => handleUpdateStatus(label.id)}
-      >
-        {label.title}
-      </button>
-    ),
-  })) || [];
+  const labelItems =
+    board['labels' + cmpId]?.map((label, idx) => ({
+      key: idx + 1,
+      backgroundcolor: label.color,
+      status: label.title,
+      label: (
+        <button
+          key={label.id}
+          className="btn-ctn medium-primary"
+          style={{
+            backgroundColor: label.color,
+            width: '152px',
+            height: '32px',
+            margin: '10px 10px 0 10px',
+          }}
+          onClick={() => handleUpdateStatus(label.id)}>
+          {label.title}
+        </button>
+      ),
+    })) || []
 
   const items = [
     ...labelItems,
@@ -70,19 +80,14 @@ export function StatusPicker({
         </div>
       ),
     },
-  ];
-
-  const selectedLabel = board['labels' + cmpId]?.find(
-    (label) => label.id === task['status' + cmpId]
-  );
+  ]
 
   return (
     <div className="cell status-picker-cell">
       <ConfigProvider
         theme={{
           boxShadow: 'none',
-        }}
-      >
+        }}>
         <Dropdown
           rootClassName="dropdown-status-picker"
           menu={{
@@ -92,21 +97,19 @@ export function StatusPicker({
           placement="bottom"
           arrow={{
             pointAtCenter: true,
-          }}
-        >
+          }}>
           <div style={{ width: '100%', height: '100%' }}>
             <button
               className="label-btn"
               style={{
                 backgroundColor: selectedLabel?.color || '#c4c4c4',
                 color: 'white',
-              }}
-            >
+              }}>
               {selectedLabel?.title || "Haven't Started"}
             </button>
           </div>
         </Dropdown>
       </ConfigProvider>
     </div>
-  );
+  )
 }
