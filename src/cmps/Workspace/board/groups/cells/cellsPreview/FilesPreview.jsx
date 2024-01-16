1b6f8a87aebdfd976bfa8bd79b406686a5fc8d20
Spiivak <edenspiivak@gmail.com>
2024-01-16 02:28:17 +0200

    Remove console logs

--- src/cmps/Workspace/board/groups/cells/cellsPreview/FilesPreview.jsx
+++ src/cmps/Workspace/board/groups/cells/cellsPreview/FilesPreview.jsx
@@ -2,18 +2,11 @@ import React from 'react';
 import emptyState from '../../../../../../assets/img/empty-state.svg';
 
 export default function FilesPreview({ activeTask, activeBoard }) {
-  console.log('FilesPreview activeTask:', activeTask);
 
-  // Get all keys of activeTask that start with "file"
-  const fileKeys = Object.keys(activeTask).filter(key => key.startsWith('file'));
+  const fileKeys = Object.keys(activeTask).filter(key => key.startsWith('file'))
 
-  // Get values corresponding to the filtered keys
-  const fileValues = fileKeys.map(key => activeTask[key]);
-  console.log('FilesPreview fileValues:', fileValues);
-
-  console.log('FilesPreview fileKeys:', fileKeys);
-  const imgUrls = fileValues.map(file => file.imgUrl);
-  console.log('FilesPreview imgUrls:', imgUrls);
+  const fileValues = fileKeys.map(key => activeTask[key])
+  const imgUrls = fileValues.map(file => file.imgUrl)
 
   return (
     <div className={`files-preview-tab ${imgUrls.length > 0 ? '' : 'flex justify-center align-center'}`}>
