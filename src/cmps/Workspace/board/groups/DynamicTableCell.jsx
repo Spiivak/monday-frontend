ee84992524438c8154400d2982ad93c146b4537a
Spiivak <edenspiivak@gmail.com>
2024-01-16 12:57:29 +0200

    remove comments

--- src/cmps/Workspace/board/groups/DynamicTableCell.jsx
+++ src/cmps/Workspace/board/groups/DynamicTableCell.jsx
@@ -8,15 +8,7 @@ import { NumbersPicker } from './cells/NumbersPicker'
 import { TaskTitle } from './cells/TaskTitle'
 import { useSelector } from 'react-redux'
 
-export function DynamicTableCell({
-  board,
-  cmp,
-  cmpId,
-  task,
-  onTaskUpdate,
-  group,
-  cmpsOrder,
-}) {
+export function DynamicTableCell({ board, cmp, cmpId, task, onTaskUpdate, group, cmpsOrder }) {
   const loggedInUser = useSelector((storeState) => storeState.userModule.user)
   async function handleUpdateTask(cmpType, data, task) {
     try {
@@ -29,50 +21,28 @@ export function DynamicTableCell({
   switch (cmp) {
     case 'title':
       return <TaskTitle {...{ task, cmpId, handleUpdateTask, cmpsOrder, group }} />
+
     case 'StatusPicker':
-      return (
-        <StatusPicker
-          {...{ task, cmpId, handleUpdateTask, cmpsOrder, board, loggedInUser }}
-        />
-      )
+      return <StatusPicker {...{ task, cmpId, handleUpdateTask, cmpsOrder, board, loggedInUser }} />
+
     case 'MemberPicker':
-      return (
-        <MemberPicker
-          {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }}
-        />
-      )
+      return <MemberPicker {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }} />
+
     case 'DatePicker':
-      return (
-        <DatePickerC
-          {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }}
-        />
-      )
+      return <DatePickerC {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }} />
+
     case 'DescriptionPicker':
-      return (
-        <DescriptionPicker
-          {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }}
-        />
-      )
+      return <DescriptionPicker {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser }} />
+
     case 'TimelinePicker':
-      return (
-        <TimelinePicker
-          {...{ task, cmpId, handleUpdateTask, group, cmpsOrder, loggedInUser }}
-        />
-      )
+      return <TimelinePicker {...{ task, cmpId, handleUpdateTask, group, cmpsOrder, loggedInUser }} />
+
     case 'FilePicker':
       return <FilePicker {...{ task, cmpId, handleUpdateTask, cmpsOrder }} />
+      
     case 'NumbersPicker':
-      return (
-        <NumbersPicker
-          {...{
-            task,
-            cmpId,
-            handleUpdateTask,
-            cmpsOrder,
-            loggedInUser,
-          }}
-        />
-      )
+      return <NumbersPicker {...{ task, cmpId, handleUpdateTask, cmpsOrder, loggedInUser, }} />
+
     default:
       return
   }
