237740514fbe5cb5fef9eed41de253db2c2ef88e
Spiivak <edenspiivak@gmail.com>
2024-01-16 11:34:23 +0200

    Fix undefined

--- src/cmps/Workspace/board/groups/cells/cellsPreview/FilesPreview.jsx
+++ src/cmps/Workspace/board/groups/cells/cellsPreview/FilesPreview.jsx
@@ -3,10 +3,13 @@ import emptyState from '../../../../../../assets/img/empty-state.svg';
 
 export default function FilesPreview({ activeTask, activeBoard }) {
 
-  const fileKeys = Object.keys(activeTask).filter(key => key.startsWith('file'))
-
-  const fileValues = fileKeys.map(key => activeTask[key])
-  const imgUrls = fileValues.map(file => file.imgUrl)
+  if(!activeTask) return
+  else {
+    const fileKeys = Object.keys(activeTask).filter(key => key.startsWith('file'))
+    
+    const fileValues = fileKeys.map(key => activeTask[key])
+    const imgUrls = fileValues.map(file => file.imgUrl)
+  }
 
   return (
     <div className={`files-preview-tab ${imgUrls.length > 0 ? '' : 'flex justify-center align-center'}`}>
