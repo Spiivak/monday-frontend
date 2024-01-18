51f11ac84b5db8d03de91e72a445391ba8c64eac
Spiivak <edenspiivak@gmail.com>
2024-01-18 09:43:43 +0200

    add render hostname

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -14,6 +14,9 @@ const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
+    window.location.hostname === '3.75.158.163' ||
+    window.location.hostname === '3.125.183.140' ||
+    window.location.hostname === '35.157.117.28' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
